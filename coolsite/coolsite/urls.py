a8a8673e61179dbd95bd86f5d5df0452754a58b4
Redocer-kl <fwydegg@yandex.ru>
2023-04-22 16:45:35 +0500

    1 session

--- coolsite/coolsite/urls.py
+++ coolsite/coolsite/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from men.views import *
+from django.urls import path, include
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('men/', include('men.urls')),
 ]
